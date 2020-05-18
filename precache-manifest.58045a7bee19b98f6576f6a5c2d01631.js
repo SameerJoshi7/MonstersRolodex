4c86675002a741293b9daac9723a40e35593401b
Sameer Joshi <sameer.joshi@synapse.com>
2020-05-18 23:28:01 +0530

    Updates

---
+++ precache-manifest.58045a7bee19b98f6576f6a5c2d01631.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea24390682f2d964503194a98af27bf6",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "19c733c581efe96125f7",
+    "url": "/MonstersRolodex/static/css/main.b73e736b.chunk.css"
+  },
+  {
+    "revision": "47dc7b65c5e80733e090",
+    "url": "/MonstersRolodex/static/js/2.4781d5d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersRolodex/static/js/2.4781d5d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c733c581efe96125f7",
+    "url": "/MonstersRolodex/static/js/main.65ad81d0.chunk.js"
+  },
+  {
+    "revision": "7172ba07cbd8bc507c88",
+    "url": "/MonstersRolodex/static/js/runtime-main.71cf3880.js"
+  }
+]);
